ee1b54948ffa51ecd315e4a88048f40073b2eb71
Azra Tabassum <azra.tabassum02@gmail.com>
2025-03-21 09:52:55 +1100

    Initial project structure added

---
+++ Mars-Competition-Nunit/DataModel/EducationModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mars_Competition_Nunit.Data_Model
+{
+    class EducationPageModel
+    {
+    }
+}
